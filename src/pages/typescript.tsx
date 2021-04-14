b9907ef0b2ab69cc8d03a87df08206e833d2f5d4
Serge Modin <modinsv@yandex.ru>
2021-04-14 22:17:58 +0300

    TypeScript installation
    - Install typescript:
    npm i -D typescript
    
    - Generate ts confing for the project
    ./node_modules/.bin/tsc --init

--- src/pages/typescript.tsx
+++ src/pages/typescript.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 
+type City = "London"
+
 const TypeScript = () => {
+  let city: City;
+  city = "New York"
+  console.log(city);
+  
   return <div>Hello world!</div>
 }
 
